922f8525ab8991b2a7d0000ff3a1004e253c5380
vpolamar <vpolamar@kent.edu>
2022-10-24 15:48:37 +0000

    Changes in Task1

--- HW2_Task1/bottle_app.py
+++ HW2_Task1/bottle_app.py
@@ -4,38 +4,38 @@ import database
 
 @route('/')
 def get_index():
-    redirect('/list')
+    redirect('/task1/list/')
 
-@route('/list')
+@route('/task1/list/')
 def get_list():
     items = database.get_items()
-    return template("shopping_list.tpl", name="Dr. DeLozier", shopping_list=items)
+    return template("shopping_list.tpl", name="pvkalyan13", shopping_list=items)
 
-@post('/add')
+@post('/task1/add/')
 def post_add():
     description = request.forms.get("description")
     database.add_item(description)
-    redirect('/list')
+    redirect('/task1/list/')
 
-@route("/delete/<id>")
+@route("/task1/delete/<id>")
 def get_delete(id):
     database.delete_item(id)
-    redirect('/list')
+    redirect('/task1/list/')
 
-@get("/edit/<id>")
+@get("/task1/edit/<id>")
 def get_edit(id):
     items = database.get_items(id)
     if len(items) != 1:
-        redirect('/list')
+        redirect('/task1/list/')
     item_id, description = items[0]['id'], items[0]['desc']
     assert item_id == int(id)
     return template("edit_item.tpl", id=id, description=description)
 
-@post("/edit/<id>")
+@post("/task1/edit/<id>")
 def post_edit(id):
     description = request.forms.get("description")
     database.update_item(id, description)
-    redirect('/list')
+    redirect('/task1/list/')
 
 application = default_app()
 
--- HW2_Task1/views/shopping_list.tpl
+++ HW2_Task1/views/shopping_list.tpl
@@ -6,13 +6,13 @@
 % for item in shopping_list:
   <tr>
     <td>{{str(item['desc'])}}</td>
-    <td><a href="/edit/{{str(item['id'])}}">edit</a></td>
-    <td><a href="/delete/{{str(item['id'])}}">delete</a></td>
+    <td><a href="/task1/edit/{{str(item['id'])}}">edit</a></td>
+    <td><a href="/task1/delete/{{str(item['id'])}}">delete</a></td>
   </tr>
 % end
 </table>
 <hr/>
-<form action="/add" method="post">
+<form action="/task1/add/" method="post">
   <p>Add new item: <input name="description"/></p>
   <p><button type="submit">Submit</button>
 </form>
