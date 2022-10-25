c2768bef875822995d5824c79002d5261540d412
vpolamar <vpolamar@kent.edu>
2022-10-25 00:58:07 +0000

    Task 1 changes

--- HW2_Task2/bottle_app.py
+++ HW2_Task2/bottle_app.py
@@ -4,14 +4,14 @@ from dataset_database import get_items, add_item, delete_item, update_item
 
 @route('/')
 def get_index():
-    redirect('/list')
+    redirect('/task2/list/')
 
-@route('/list')
+@route('/task2/list/')
 def get_list():
     items = get_items()
-    return template("shopping_list.tpl", name="Dr. DeLozier", shopping_list=items)
+    return template("shopping_list.tpl", name="pvkalyan13", shopping_list=items)
 
-@post('/add')
+@post('/task2/add/')
 def post_add():
     description = request.forms.get("description")
     quantity = request.forms.get("quantity")
@@ -20,27 +20,27 @@ def post_add():
     except:
         quantity = 1
     add_item(description, quantity)
-    redirect('/list')
+    redirect('/task2/list/')
 
-@route("/delete/<id>")
+@route("/task2/delete/<id>")
 def get_delete(id):
     delete_item(id)
-    redirect('/list')
+    redirect('/task2/list/')
 
-@get("/edit/<id>")
+@get("/task2/edit/<id>")
 def get_edit(id):
     items = get_items(id)
     if len(items) != 1:
-        redirect('/list')
+        redirect('/task2/list/')
     item_id, description = items[0]['id'], items[0]['description']
     assert item_id == int(id)
     return template("edit_item.tpl", id=id, description=description)
 
-@post("/edit/<id>")
+@post("/task2/edit/<id>")
 def post_edit(id):
     description = request.forms.get("description")
     update_item(id, description)
-    redirect('/list')
+    redirect('/task2/list/')
 
 application = default_app()
 
